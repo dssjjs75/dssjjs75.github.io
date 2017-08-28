c1e2d18bf682647a39c710c7fa7eb42bc3ce478a
Git user <username@users.noreply.github.com>
2017-08-28 17:05:36 +0100

    x

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     //add responsive tag to all images
     $(".blog-post").find("img").addClass("img-responsive");
     $(".blog-post").find("img").width("80%");
-
+    alert('dog');
     //change width of sidebar if columns stacked
     if($(window).width() <= 1080) {
         alert('hello'); 
