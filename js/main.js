be5c4a5ae8dae68d24169091b5f0715bf56f3609
Git user <username@users.noreply.github.com>
2017-08-28 13:44:54 +0100

    x

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    //add responsive tag to all images
+    $(".blog-post").find("img").addClass("img-responsive");
+    $(".blog-post").find("img").width("80%");
+
+});
