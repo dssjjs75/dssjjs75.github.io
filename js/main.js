d5c00fd429a94c71835a59e7014171230b6f97c1
Git user <username@users.noreply.github.com>
2017-08-28 17:10:53 +0100

    x

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+function onresize()
+{
+    if($(window).width() <= 1080) {
+        $(".col-sm-3").width("100%"); 
+    }
+}
+
 $( document ).ready(function() {
     //add responsive tag to all images
     $(".blog-post").find("img").addClass("img-responsive");
     $(".blog-post").find("img").width("80%");
     //change width of sidebar if columns stacked
-    if($(window).width() <= 1080) {
-        $(".col-sm-3").width("100%"); 
-    }
-
+    onresize();
+    $(window).resize(onresize);
 });
